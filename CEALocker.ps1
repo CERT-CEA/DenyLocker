86c816799559284a6e358acc46b147f01596f06b
MARTIN Florian <20000353+ipfyx@users.noreply.github.com>
2024-02-21 09:32:30 +0100

    v2

--- CEALocker.ps1
+++ CEALocker.ps1
@@ -4,14 +4,16 @@
     Les règles autorise tout sauf la liste de logiciels présents dans différents dossiers
     
     .PARAMETER OutFile
-    Fichier de sortie json de la configuration sysmon
+    Nom du fichier de sortie xml Applocker voulu
 
     .NOTES
-    (c) Florian MARTIN 2022
+    (c) Florian MARTIN 2023
     version 1.0
 
     Example
-    .\CEALocker.ps1 -OutFile output.xml
+    .\CEALocker.ps1 -OutFile example.xml
+    Fichier de sortie par défaut : yyyyMMdd_cealocker.xml
+    .\CEALocker.ps1
 
 #>
 
@@ -22,6 +24,8 @@ $rootDir = [System.IO.Path]::GetDirectoryName($MyInvocation.MyCommand.Path)
 . $rootDir\Config.ps1
 . $rootDir\Init.ps1
 $xDocument = [xml](Get-Content $defRulesXml)
+
+# Code from AaronLocker
 function SaveXmlDocAsUnicode([System.Xml.XmlDocument] $xmlDoc, [string] $xmlFilename)
 {
     $xws = [System.Xml.XmlWriterSettings]::new()
@@ -32,12 +36,24 @@ function SaveXmlDocAsUnicode([System.Xml.XmlDocument] $xmlDoc, [string] $xmlFile
     $xw.Close()
 }
 
+# Code from AaronLocker
 function SaveAppLockerPolicyAsUnicodeXml([Microsoft.Security.ApplicationId.PolicyManagement.PolicyModel.AppLockerPolicy]$ALPolicy, [string]$xmlFilename)
 {
     SaveXmlDocAsUnicode -xmlDoc ([xml]($ALPolicy.ToXml())) -xmlFilename $xmlFilename
 }
 
-function CreateFilePublisherCondition($xDocument, $publisher, $filename, $rule) {
+function CreateFilePublisherCondition {
+    Param(
+        [Parameter(Mandatory = $true)] $xDocument,
+        [Parameter(Mandatory = $true)] $publisher,
+        [Parameter(Mandatory = $true)] [string] $filename,
+        [Parameter(Mandatory = $true)] $rule
+    )
+    # Create a FilePublisherCondition element
+    # <FilePublisherCondition PublisherName="O=MULLVAD VPN AB, L=GÖTEBORG, C=SE" ProductName="MULLVAD VPN" BinaryName="*">
+    # <BinaryVersionRange LowSection="*" HighSection="*" />
+    # This XML is used between <Exceptions> OR <FilePublisherRule>
+
     $filePublisherCondition = $xDocument.CreateElement("FilePublisherCondition")   
     if ($publisher.ProductName) {
         $msg = "Building {0} rule for group {1} for {2} IN {3}" -f $rule.action, $rule.UserOrGroup, $publisher.ProductName, $rule.directory
@@ -48,7 +64,7 @@ function CreateFilePublisherCondition($xDocument, $publisher, $filename, $rule) 
 
     $filePublisherCondition.SetAttribute("PublisherName", $publisher.PublisherName)
     
-    if ($rule.denyProduct -and $publisher.ProductName) {
+    if ($rule.ruleProduct -and $publisher.ProductName) {
         $filePublisherCondition.SetAttribute("ProductName", $publisher.ProductName)
     } else {
         $filePublisherCondition.SetAttribute("ProductName", "*" )
@@ -69,81 +85,154 @@ function CreateFilePublisherCondition($xDocument, $publisher, $filename, $rule) 
     Return $filePublisherCondition
 }
 
-function CreateFilePathCondition($xDocument, $filename, $rule) {
+function CreateFilePathCondition {
+    Param(
+        [Parameter(Mandatory = $true)] $xDocument,
+        [Parameter(Mandatory = $true)] [string] $filename,
+        [Parameter(Mandatory = $true)] $rule
+    )
+    # Create a FilePathCondition element
+    # <FilePathCondition Path="ngrok*.exe" />
+    # This XML is used between <Exceptions> OR <FilePathRule>
     $filePathCondition = $xDocument.CreateElement("FilePathCondition")   
     $filePathCondition.SetAttribute("Path", $filename+"*.exe")
-    $msg = "Building {0} rule for group {1} for {2} IN {3} based on filename" -f $rule.action, $rule.UserOrGroup, $filename, $rule.directory
+    $msg = "Building {0} rule for group {1} for software {2} IN {3} based on filename" -f $rule.action, $rule.UserOrGroup, $filename, $rule.directory
     Write-Warning $msg
     Return $filePathCondition
 }
 
-foreach ($rule in $denyRules) {
+function CreateFilePublisherRule {
+    Param(
+        [Parameter(Mandatory = $true)] $xDocument,
+        [Parameter(Mandatory = $true)] $publisher,
+        [Parameter(Mandatory = $true)] [string] $filename,
+        [Parameter(Mandatory = $true)] $rule
+    )
+    # Create a FilePublisherRule element
+    # <FilePublisherRule Action="Deny" UserOrGroupSid="S-1-1-0" Description="" Name="DropBox">
+    #   <Conditions>
+    #      ...
+    #   </Conditions>
+    # </FilePublisherRule>
+    $fileRule = $xDocument.CreateElement("FilePublisherRule")
 
-    Get-ChildItem $rule.directory -File | ForEach-Object {
+    if ($publisher.ProductName) {
+        $fileRule.SetAttribute("Description", $publisher.ProductName)
+        $fileRule.SetAttribute("Name", $rule.action + " " + $rule.UserOrGroup + " " + $publisher.ProductName)
+    } else {
+        $fileRule.SetAttribute("Description", $filename)
+        $fileRule.SetAttribute("Name", $rule.action + " " + $rule.UserOrGroup + " " + $filename)
+    }
+    Return $fileRule
+}
 
-        $publisher = (Get-AppLockerFileInformation $_.FullName).Publisher
-        $filename = $_.Name.split('.')[0]
+function CreateFilePathRule {
+    Param(
+        [Parameter(Mandatory = $true)] $xDocument,
+        [Parameter(Mandatory = $true)] [string] $filename,
+        [Parameter(Mandatory = $true)] $rule
+    )
+    # Create a FilePathRule element
+    # <FilePathRule Action="Deny" UserOrGroupSid="S-1-1-0" Description="" Name="DropBox">
+    #      ...
+    # </FilePathRule>
+    $fileRule = $xDocument.CreateElement("FilePathRule")
+    $fileRule.SetAttribute("Description", $filename)
+    $fileRule.SetAttribute("Name", $rule.action + " " + $rule.UserOrGroup + " " + $filename)
+    Return $fileRule
+}
 
-        $xDenyPlaceholder = $xDocument.SelectNodes("//PLACEHOLDER_EXETODENY")[0]
-        $xDeny = $xDenyPlaceholder.ParentNode
-    
-        if ($null -eq $publisher)
-        {
-            Write-Warning "UNABLE TO BUILD DENYLIST RULE BASED ON SIGNATURE FOR $_"
-            # Create a FilePathRule element
-            # <FilePathRule Action="Deny" UserOrGroupSid="S-1-1-0" Description="" Name="DropBox">
-            $fileRule = $xDocument.CreateElement("FilePathRule")
-            $fileRule.SetAttribute("Description", $filename)
-            $fileRule.SetAttribute("Name", $rule.action + " " + $rule.UserOrGroup + " " + $filename)
+function CreateRule {
 
-            # Create a FilePathCondition element
-            # <FilePathCondition FilePath="ngrok">
-            $fileCondition = CreateFilePathCondition $xDocument $filename $rule
+    Param(
+        [Parameter(Mandatory = $true)] $xDocument,
+        [Parameter(Mandatory = $true)] [ValidateScript( { $_ -in $applockerRuleCollection } )] [string] $ruleCollection,
+        [Parameter(Mandatory = $true)] [array] $rules,
+        [Parameter(Mandatory = $true)] [ValidateScript( { $_ -in $placeholders.Keys } )] [string] $placeholderKey
+    )
+    $msg = "Building ruleCollection {0} {1} rules" -f $ruleCollection, $placeholderKey
+    Write-Host $msg -ForegroundColor Green
+
+    # Let's create an XML child
+    $xPlaceholder = $xDocument.SelectNodes("//"+$placeholders.Item($placeholderKey))[0]
+    $xPlaceholderParentNode = $xPlaceholder.ParentNode
+
+    foreach ($rule in $rules) {
+
+        # We want to block MSI file installation if the product has one
+        if ($ruleCollection -eq "Msi") {
+            $files = Get-ChildItem $rule.directory -File -Filter "*.msi"
+        # But also the resulting EXE, so we want EXE AND MSI files signature
+        } else {
+            $files = Get-ChildItem $rule.directory -File 
         }
-        else
-        {
-            # Create a FilePublisherRule element
-            # <FilePublisherRule Action="Deny" UserOrGroupSid="S-1-1-0" Description="" Name="DropBox">
-            $fileRule = $xDocument.CreateElement("FilePublisherRule")
-
-            if ($publisher.ProductName) {
-                $fileRule.SetAttribute("Description", $publisher.ProductName)
-                $fileRule.SetAttribute("Name", $rule.action + " " + $rule.UserOrGroup + " " + $publisher.ProductName)
-            } else {
-                $fileRule.SetAttribute("Description", $filename)
-                $fileRule.SetAttribute("Name", $rule.action + " " + $rule.UserOrGroup + " " + $filename)
+        $files | ForEach-Object {
+    
+            $publisher = (Get-AppLockerFileInformation $_.FullName).Publisher
+            $filename = $_.Name.split('.')[0]
+            
+            if ($null -eq $publisher)
+            {
+                Write-Warning "UNABLE TO BUILD DENYLIST RULE BASED ON SIGNATURE FOR $_"
+                $fileRule = CreateFilePathRule $xDocument $filename $rule
+                # Create a FilePathCondition element
+                # <FilePathCondition FilePath="ngrok">
+                $fileCondition = CreateFilePathCondition $xDocument $filename $rule
+            }
+            else
+            {
+                $fileRule = CreateFilePublisherRule $xDocument $publisher $filename $rule
+                # Create a FilePublisherCondition element
+                # <FilePublisherCondition BinaryName="*" ProductName="*" PublisherName="O=DROPBOX, INC, L=SAN FRANCISCO, S=CALIFORNIA, C=US">
+                $fileCondition = CreateFilePublisherCondition $xDocument $publisher $filename $rule
             }
 
-            # Create a FilePublisherCondition element
-            # <FilePublisherCondition BinaryName="*" ProductName="*" PublisherName="O=DROPBOX, INC, L=SAN FRANCISCO, S=CALIFORNIA, C=US">
-            $fileCondition = CreateFilePublisherCondition $xDocument $publisher $filename $rule
+            if ($rule.action -eq "Deny") {
+                $fileRule.SetAttribute("Action", $rule.action)
+                $fileRule.SetAttribute("UserOrGroupSid", $rule.UserOrGroupSid)
+                $fileRule.SetAttribute("Id", ([guid]::NewGuid()).Guid)
+                # Create a Conditions element
+                # <Conditions>
+                $condition = $xDocument.CreateElement("Conditions")
+                $condition.AppendChild($fileCondition) | Out-Null
+                $fileRule.AppendChild($condition) | Out-Null
+                # Add the publisher condition where the placeholder is
+                $xPlaceholderParentNode.AppendChild($fileRule) | Out-Null
+            } else {
+                $xPlaceholderParentNode.AppendChild($fileCondition) | Out-Null
+            }
+        } 
+    }
+    # Remove placeholder elements
+    $xPlaceholderParentNode.RemoveChild($xPlaceholder) | Out-Null
+}
+
+function TestRule {
+
+    Param(
+        [Parameter(Mandatory = $true)] [ValidateScript( { $_ -in $applockerRuleCollection } )] [string] $ruleCollection,
+        [Parameter(Mandatory = $true)] [array] $rules,
+        [Parameter(Mandatory = $true)] $applockerPolicy
+    )
+
+    foreach ($rule in $rules) {
+        Get-ChildItem $rule.directory -File | ForEach-Object {
+            $_  |Convert-Path| Test-AppLockerPolicy -XmlPolicy $applockerPolicy|Select FilePath, PolicyDecision
         }
-
-        $fileRule.SetAttribute("Action", $rule.action)
-        $fileRule.SetAttribute("UserOrGroupSid", $rule.UserOrGroupSid)
-        $fileRule.SetAttribute("Id", ([guid]::NewGuid()).Guid)
-
-        # Create a Conditions element
-        # <Conditions>
-        $condition = $xDocument.CreateElement("Conditions")
-
-        $condition.AppendChild($fileCondition) | Out-Null
-        $fileRule.AppendChild($condition) | Out-Null
-        # Add the publisher condition where the placeholder is
-        $xDeny.AppendChild($fileRule) | Out-Null
     }
 
 }
 
-$xAllowExceptPlaceholder = $xDocument.SelectNodes("//PLACEHOLDER_EXETODENY_EXCEPTION")[0]
-$xAllowExcept = $xAllowExceptPlaceholder.ParentNode
+#CreateRule $xDocument "Exe" $denyRules "EXE DENY"
+#CreateRule $xDocument "Msi" $denyRules "MSI DENY"
+#CreateRule $xDocument "Exe" $allowExceptDenyRule "EXE EXCEPTION"
 
-# Remove the placeholder element
-$xDeny.RemoveChild($xDenyPlaceholder) | Out-Null
-$xAllowExcept.RemoveChild($xAllowExceptPlaceholder) | Out-Null
+TestRule "Exe" $denyRules $OutFile
+TestRule "Exe" $allowExceptDenyRule $OutFile
 
-Write-Host $xDocument.OuterXml
+Write-Debug $xDocument.OuterXml
 
-$masterPolicy = [Microsoft.Security.ApplicationId.PolicyManagement.PolicyModel.AppLockerPolicy]::FromXml($xDocument.OuterXml)
+#$masterPolicy = [Microsoft.Security.ApplicationId.PolicyManagement.PolicyModel.AppLockerPolicy]::FromXml($xDocument.OuterXml)
 
 SaveAppLockerPolicyAsUnicodeXml -ALPolicy $masterPolicy -xmlFilename $rulesFileEnforceNew
+
--- Config.ps1
+++ Config.ps1
@@ -1,39 +1,34 @@
 $defRulesXml = [System.IO.Path]::Combine($rootDir, "template.xml")
 $rulesFileEnforceNew = [System.IO.Path]::Combine($rootDir, $OutFile)
 
-$everyonePublisherToDenyDir = [System.IO.Path]::Combine($rootDir, "everyonePublisherToDenyTest")
-$everyoneProductToDenyDir = [System.IO.Path]::Combine($rootDir, "everyoneProductToDenyTest")
-$everyoneBinaryToDenyDir = [System.IO.Path]::Combine($rootDir, "everyoneBinaryToDenyTest")
+$everyoneProductToDenyDir = [System.IO.Path]::Combine($rootDir, "everyoneProductToDeny")
+$everyoneBinaryToDenyDir = [System.IO.Path]::Combine($rootDir, "everyoneBinaryToDeny")
 
-$adminProductToDenyDir = [System.IO.Path]::Combine($rootDir, "adminProductToDenyTest")
+$adminProductToDenyDir = [System.IO.Path]::Combine($rootDir, "adminProductToDeny")
 
-$anyProductToDenyExceptionDir = [System.IO.Path]::Combine($rootDir, "anyProductToDenyExceptionTest")
+$anyProductToDenyExceptionDir = [System.IO.Path]::Combine($rootDir, "anyProductToDenyException")
+
+$placeholders = @{"EXE DENY" = "PLACEHOLDER_EXETODENY"; "MSI DENY" = "PLACEHOLDER_MSITODENY"; "EXE EXCEPTION" = "PLACEHOLDER_EXETODENY_EXCEPTION"}
+
+$applockerRuleCollection = ('Exe','Script','MSI','Dll','Appx')
 
 # Génére les paramètres de la règle de blocage pour chaque dossier
-# everyonePublisherToDeny : bloque tous les exécutables du publisher du binaire pour Everyone
-# everyoneProductToDeny : bloque seulement le produit donné pour Everyone
+# everyoneProductToDeny : bloque le publishier et le produit pour Everyone
 # everyoneBinaryToDeny : inutilisé
 # adminProductToDeny : bloque seulement le binaire donné pour les membres du groupe Administrators
 # anyProductToDenyException : bloque seulement le produit donné pour Everyone, mais le produit peut être autorisé dans une règle Allow par ailleurs
 
-$everyonePublisherToDenyRule = New-Object PSObject
-$everyonePublisherToDenyRule |Add-Member Noteproperty denyPublisher $true
-$everyonePublisherToDenyRule |Add-Member Noteproperty directory $everyonePublisherToDenyDir
-$everyonePublisherToDenyRule |Add-Member Noteproperty action "Deny"
-$everyonePublisherToDenyRule |Add-Member Noteproperty UserOrGroupSid "S-1-1-0"
-$everyonePublisherToDenyRule |Add-Member Noteproperty UserOrGroup "Everyone"
-
 $everyoneProductToDenyRule = New-Object PSObject
-$everyoneProductToDenyRule |Add-Member Noteproperty denyPublisher $true
-$everyoneProductToDenyRule |Add-Member Noteproperty denyProduct $true
+$everyoneProductToDenyRule |Add-Member Noteproperty rulePublisher $true
+$everyoneProductToDenyRule |Add-Member Noteproperty ruleProduct $true
 $everyoneProductToDenyRule |Add-Member Noteproperty directory $everyoneProductToDenyDir
 $everyoneProductToDenyRule |Add-Member Noteproperty action "Deny"
 $everyoneProductToDenyRule |Add-Member Noteproperty UserOrGroupSid "S-1-1-0"
 $everyoneProductToDenyRule |Add-Member Noteproperty UserOrGroup "Everyone"
 
 $everyoneBinaryToDenyRule = New-Object PSObject
-$everyoneBinaryToDenyRule |Add-Member Noteproperty denyPublisher $true
-$everyoneBinaryToDenyRule |Add-Member Noteproperty denyProduct $true
+$everyoneBinaryToDenyRule |Add-Member Noteproperty rulePublisher $true
+$everyoneBinaryToDenyRule |Add-Member Noteproperty ruleProduct $true
 $everyoneBinaryToDenyRule |Add-Member Noteproperty denyBinary $true
 $everyoneBinaryToDenyRule |Add-Member Noteproperty directory $everyoneBinaryToDenyDir
 $everyoneBinaryToDenyRule |Add-Member Noteproperty action "Deny"
@@ -41,18 +36,20 @@ $everyoneBinaryToDenyRule |Add-Member Noteproperty UserOrGroupSid "S-1-1-0"
 $everyoneBinaryToDenyRule |Add-Member Noteproperty UserOrGroup "Everyone"
 
 $adminProductToDenyRule = New-Object PSObject
-$adminProductToDenyRule |Add-Member Noteproperty denyPublisher $true
-$adminProductToDenyRule |Add-Member Noteproperty denyProduct $true
+$adminProductToDenyRule |Add-Member Noteproperty rulePublisher $true
+$adminProductToDenyRule |Add-Member Noteproperty ruleProduct $true
 $adminProductToDenyRule |Add-Member Noteproperty directory $adminProductToDenyDir
 $adminProductToDenyRule |Add-Member Noteproperty action "Deny"
 $adminProductToDenyRule |Add-Member Noteproperty UserOrGroupSid "S-1-5-32-544"
 $adminProductToDenyRule |Add-Member Noteproperty UserOrGroup "Administrators"
 
 $allowExceptDenyRule  = New-Object PSObject
+$allowExceptDenyRule |Add-Member Noteproperty rulePublisher $true
+$allowExceptDenyRule |Add-Member Noteproperty ruleProduct $true
 $allowExceptDenyRule  |Add-Member Noteproperty directory $anyProductToDenyExceptionDir
 $allowExceptDenyRule  |Add-Member Noteproperty action "Allow"
 $allowExceptDenyRule  |Add-Member Noteproperty UserOrGroupSid "S-1-1-0"
 $allowExceptDenyRule  |Add-Member Noteproperty UserOrGroup "Everyone"
 
-$denyRules = ($everyonePublisherToDenyRule, $everyoneProductToDenyRule, $everyoneBinaryToDenyRule, 
+$denyRules = ($everyoneProductToDenyRule, $everyoneBinaryToDenyRule, 
                         $adminProductToDenyRule)
