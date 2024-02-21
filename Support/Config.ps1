834ecf968ebedbcceb15f067099e4efdaf58e108
MARTIN Florian <20000353+ipfyx@users.noreply.github.com>
2024-02-21 09:32:34 +0100

    ajout des policy SCRIPT MSI et EXE pour les PATH

--- Support/Config.ps1
+++ Support/Config.ps1
@@ -4,11 +4,24 @@ $defRulesXml = [System.IO.Path]::Combine($supportDir, "template.xml")
 $ps1_ExportPolicyToCSV = [System.IO.Path]::Combine($supportDir, "ExportPolicy-ToCsv.ps1")
 $ps1_ExportPolicyToExcel = [System.IO.Path]::Combine($supportDir, "ExportPolicy-ToExcel.ps1")
 
-$placeholders = @{"EXE DENY" = "PLACEHOLDER_EXETODENY"; 
-                "EXE ALLOW" = "PLACEHOLDER_EXETOALLOW";
-                "EXE EXCEPTION" = "PLACEHOLDER_EXETODENY_EXCEPTION" ; 
-                "MSI DENY" = "PLACEHOLDER_MSITODENY"; 
-                "MSI ALLOW" = "PLACEHOLDER_MSITOALLOW";
-                "MSI EXCEPTION" = "PLACEHOLDER_MSITODENY_EXCEPTION" ;}
+$placeholders = @{"EXE PRODUCT DENY" = "PLACEHOLDER_EXE_PRODUCT_DENY"; 
+                "EXE PRODUCT ALLOW" = "PLACEHOLDER_EXE_PRODUCT_ALLOW";
+                "EXE PRODUCT DENY EXCEPTION" = "PLACEHOLDER_EXE_PRODUCT_DENY_EXCEPTION" ;
+
+                "EXE PATH DENY" = "PLACEHOLDER_EXE_PATH_DENY"; 
+                "EXE PATH ALLOW" = "PLACEHOLDER_EXE_PATH_ALLOW"; 
+                "EXE PATH DENY EXCEPTION" = "PLACEHOLDER_EXE_PATH_DENY_EXCEPTION";
+
+                "MSI PRODUCT DENY" = "PLACEHOLDER_MSI_PRODUCT_DENY"; 
+                "MSI PRODUCT ALLOW" = "PLACEHOLDER_MSI_PRODUCT_ALLOW";
+                "MSI PRODUCT DENY EXCEPTION" = "PLACEHOLDER_MSI_PRODUCT_DENY_EXCEPTION" ;
+
+                "MSI PATH DENY" = "PLACEHOLDER_MSI_PATH_DENY"; 
+                "MSI PATH ALLOW" = "PLACEHOLDER_MSI_PATH_ALLOW";
+                "MSI PATH DENY EXCEPTION" = "PLACEHOLDER_MSI_PATH_DENY_EXCEPTION" ;
+
+                "SCRIPT PATH DENY" = "PLACEHOLDER_SCRIPT_PATH_DENY";
+                "SCRIPT PATH ALLOW" = "PLACEHOLDER_SCRIPT_PATH_ALLOW";
+                "SCRIPT PATH DENY EXCEPTION" = "PLACEHOLDER_SCRIPT_PATH_DENY_EXCEPTION";}
 
 $applockerRuleCollection = ('Exe','Script','MSI','Dll','Appx')
