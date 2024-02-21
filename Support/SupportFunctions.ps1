c3c1b1056cb50efb235c3fd9e0863be99564fc3d
MARTIN Florian <20000353+ipfyx@users.noreply.github.com>
2024-02-21 09:32:37 +0100

    deplacement ReadJson dans Support

--- Support/SupportFunctions.ps1
+++ Support/SupportFunctions.ps1
@@ -508,6 +508,25 @@ function RenamePaths($paths, $forUsername)
     }
 }
 
+####################################################################################################
+# Json stuff
+####################################################################################################
+function ReadJson {
+    Param(
+        [Parameter(Mandatory = $true)] [string] $filepath
+    )
+    try {
+        $jsonContent = Get-Content -Raw -Path $filepath
+        $configData = $jsonContent | ConvertFrom-Json
+        $configData
+    } catch [System.ArgumentException] {
+        $_.Exception.GetType().FullName
+        $msg = "'{0}' is not a valid json file" -f $configFile
+        Write-Error $msg
+    }
+}
+
+
 
 ####################################################################################################
 # Global variables - known file extensions
